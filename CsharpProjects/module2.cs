b50e572f9856fb90330ce28c0820a8c64b2c50a3
jihadbaydoun <joebaydounnyc@gmail.com>
2026-02-08 21:44:16 -0500

    module2

--- CsharpProjects/module2.cs
+++ CsharpProjects/module2.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Hello, c#!");
 Random dice = new Random();  //ClassName   objectName = new ClassName();
 int roll = dice.Next(1,7);
 Console.WriteLine(roll);
-*/
+
 
 int number = 7;
 string text = "seven";
@@ -16,3 +16,12 @@ Console.WriteLine(number);
 Console.WriteLine("The number is ");
 Console.WriteLine(text);
 
+*/
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
