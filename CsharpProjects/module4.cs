2e328d41d4c25bb5bb83c989c8f89f688f6b617a
jihadbaydoun <joebaydounnyc@gmail.com>
2026-02-09 00:59:08 -0500

    v1

---
+++ CsharpProjects/module4.cs
@@ -0,0 +1,8 @@
+/*string[] fraudulentOrderIDs = new string[3];  ### this how i declarte array of dting with 3 elements, 0,1,2
+*/
+
+string[] names = { "jihad", "lewii", "Brahim" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
