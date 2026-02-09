ce2d180af038f66a8804ef6eb6360c7a8eba3cdf
jihadbaydoun <joebaydounnyc@gmail.com>
2026-02-09 00:49:24 -0500

    subscription version

--- CsharpProjects/module3.cs
+++ CsharpProjects/module3.cs
@@ -25,7 +25,7 @@ if (message.Contains("fox"))
 {
     Console.WriteLine("What does the fox say?");
 }
-*/
+/
                     /////########this the third part
 Random dice = new Random();
 
@@ -57,4 +57,35 @@ if (total >= 15)
 if (total < 15)
 {
     Console.WriteLine("Sorry, you lose.");
+}
+*/
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Expiration message
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1) //If the user's subscription will expire in one day
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+// Discount message (separate if statement)
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
 }
