b464230d79108eb21b28b3108d5891e404030587
jihadbaydoun <joebaydounnyc@gmail.com>
2026-02-08 22:08:16 -0500

    final version of mudule 2

--- CsharpProjects/module2.cs
+++ CsharpProjects/module2.cs
@@ -1,14 +1,14 @@
 ﻿//className.methodMtetode();        that how you call a method in c#
 
 
-/*
-Console.WriteLine("Hello, c#!");
 
+Console.WriteLine("Hello, c#!");
+                 //###### part 1 of the module ######
 Random dice = new Random();  //ClassName   objectName = new ClassName();
 int roll = dice.Next(1,7);
 Console.WriteLine(roll);
 
-
+                    //##### part 2 of the module ######
 int number = 7;
 string text = "seven";
 
@@ -16,8 +16,8 @@ Console.WriteLine(number);
 Console.WriteLine("The number is ");
 Console.WriteLine(text);
 
-*/
-Random dice = new Random();
+                        //###### part 3 of the module ######
+//Random dice = new Random();  // i put this one a comment because would run since we alresady declared it berfore
 int roll1 = dice.Next();
 int roll2 = dice.Next(101);
 int roll3 = dice.Next(50, 101);
@@ -25,3 +25,13 @@ int roll3 = dice.Next(50, 101);
 Console.WriteLine($"First roll: {roll1}");
 Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}");
+
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = System.Math.Max(firstValue, secondValue);
+
+
+Console.WriteLine(largerValue);                   
