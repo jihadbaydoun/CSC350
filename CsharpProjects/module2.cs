3ac8bc75a193c625aed59e979d37ee46e825159f
jihadbaydoun <joebaydounnyc@gmail.com>
2026-03-01 23:46:21 -0500

    Restored deleted modules safely

---
+++ CsharpProjects/module2.cs
@@ -0,0 +1,37 @@
+﻿//className.methodMtetode();        that how you call a method in c#
+
+
+
+Console.WriteLine("Hello, c#!");
+                 //###### part 1 of the module ######
+Random dice = new Random();  //ClassName   objectName = new ClassName();
+int roll = dice.Next(1,7);
+Console.WriteLine(roll);
+
+                    //##### part 2 of the module ######
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number);
+Console.WriteLine("The number is ");
+Console.WriteLine(text);
+
+                        //###### part 3 of the module ######
+//Random dice = new Random();  // i put this one a comment because would run since we alresady declared it berfore
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = System.Math.Max(firstValue, secondValue);
+
+
+Console.WriteLine(largerValue);                   
